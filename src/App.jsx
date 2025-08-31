c0256f43697e7f8f905f8f0ebfb0c604baef9daf
dantrixxheron <danielacmj.04@gmail.com>
2025-08-30 20:04:32 -0600

    Feat: register functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import useAuth from './hooks/useAuth'
+import useLastVisited from './hooks/useLastVisited'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const { isAuthenticated, user } = useAuth()
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+  const { setLastPath, getLastPath } = useLastVisited()
+
+  useEffect(() => { setLastPath(pathname) }, [pathname, setLastPath])
+
+  useEffect(() => {
+    if (window.location.pathname === '/') {
+      const last = getLastPath()
+      if (last && isAuthenticated() && last !== '/') navigate(last, { replace: true })
+    }
+  }, [isAuthenticated, getLastPath, navigate])
 
   return (
-    <>
-      <div>
-        <h1> Hello world</h1>
-      </div>
-    </>
+    <div className='min-h-screen'>
+      <Navbar />
+      <main className="container-app py-8">
+              <Routes>
+                <Route path='/' element={<Login/>} />
+                <Route path='/register' element={<Register/>} />
+              </Routes>
+      </main>
+    </div>
   )
 }
-
-export default App
